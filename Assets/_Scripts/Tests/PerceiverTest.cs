ab84f2f369b23d45728f696179f7075beec81dca
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-07-16 11:39:58 +0200

    ADD: [Prefabs] Test cases

---
+++ Assets/_Scripts/Tests/PerceiverTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ringo.AIPerception.Implementations;
+using ringo.AIPerception.Senses;
+using UnityEngine;
+
+namespace _Scripts.Tests
+{
+    public class PerceiverTest : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            GetComponent<MonoAIPerception>().OnPerceived += HandlePerceived;
+        }
+        
+        private void OnDisable()
+        {
+            GetComponent<MonoAIPerception>().OnPerceived -= HandlePerceived;
+        }
+        
+        private void HandlePerceived(Type perceptionType, IPerceptionData perceptionData)
+        {
+            Debug.Log("Perceived: " + perceptionType.Name + " with data: " + perceptionData);
+        }
+    }
+}
---
+++ Assets/_Scripts/Tests/Test.cs
@@ -0,0 +1,18 @@
+﻿using ringo.AIPerception.Senses.Hearing;
+using ringo.AIPerceptionSystem;
+using UnityEngine;
+
+namespace _Scripts.Tests
+{
+    public class Test : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                MonoSingletonAIPerceptionSystem.Instance.Alert<HearingSenseSO>(new HearingPerceptionData
+                    { Position = new System.Numerics.Vector3(transform.position.x, transform.position.y, transform.position.z) });
+            }
+        }
+    }
+}
