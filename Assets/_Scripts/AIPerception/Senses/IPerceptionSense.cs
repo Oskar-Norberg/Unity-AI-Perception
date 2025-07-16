2350f65c999c879dad5e5ce973968714e058945e
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-07-16 11:37:56 +0200

    ADD: [Script] IPerceptionSense

---
+++ Assets/_Scripts/AIPerception/Senses/IPerceptionSense.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ringo.AIPerception.Senses
+{
+    public interface IPerceptionSense
+    {
+        // TODO: I don't love sending a raw transform here. Inherently linked to Unity, so harder for Unit tests.
+        bool ConditionMet(Transform perceiver, IPerceptionData perceptionData);
+    }
+}
