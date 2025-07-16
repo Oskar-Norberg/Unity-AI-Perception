d8dc744692fc035f5da9fbc3f29d051021a61383
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-07-16 13:13:11 +0200

    EDIT: [Script] Properly cast to data

--- Assets/_Scripts/AIPerception/Senses/IPerceptionSense.cs
+++ Assets/_Scripts/AIPerception/Senses/IPerceptionSense.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 namespace ringo.AIPerception.Senses
 {
-    public interface IPerceptionSense<T>  where T : IAISense
+    
+    public interface IPerceptionSense<TSense, TSenseData> where TSense : IAISense where TSenseData : IPerceptionData
     {
         // TODO: I don't love sending a raw transform here. Inherently linked to Unity, so harder for Unit tests.
         bool ConditionMet(Transform perceiver, IPerceptionData perceptionData);
@@ -13,6 +14,7 @@ namespace ringo.AIPerception.Senses
     public interface IPerceptionSense
     {
         public Type SenseType { get; }
+        public Type DataType { get; }
         
         bool ConditionMet(Transform perceiver, IPerceptionData perceptionData);
     }
