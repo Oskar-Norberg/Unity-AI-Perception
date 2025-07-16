5e02969a08e611d8b6b29fe0949e8b07b02bba67
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-07-16 11:39:08 +0200

    ADD: [Script] AISenseSO

---
+++ Assets/_Scripts/AIPerception/Senses/AISenseSO.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace ringo.AIPerception.Senses
+{
+    public abstract class AISenseSO : ScriptableObject, IPerceptionSense
+    {
+        public abstract bool ConditionMet(Transform perceiver, IPerceptionData perceptionData);
+    }
+}
