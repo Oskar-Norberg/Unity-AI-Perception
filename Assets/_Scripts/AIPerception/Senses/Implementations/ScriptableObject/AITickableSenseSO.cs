6b13b8830bff0e11cc65be066ae8366ed4542a4a
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-07-16 15:27:25 +0200

    ADD: [Script] TickableSense

---
+++ Assets/_Scripts/AIPerception/Senses/Implementations/ScriptableObject/AITickableSenseSO.cs
@@ -0,0 +1,17 @@
+﻿namespace ringo.AIPerception.Senses
+{
+    public interface ITickableSense
+    {
+        void Tick(float deltaTime);
+    }
+    
+    public abstract class AITickableSenseSO<T1, T2> :  AISenseSOBase, ITickableSense, IPerceptionSense<T1, T2> where T1 : IAISense where T2 : IPerceptionData
+    {
+        public abstract void Tick(float deltaTime);
+    }
+
+    public abstract class AITickableSenseSOBase : AISenseSOBase, ITickableSense
+    {
+        public abstract void Tick(float deltaTime);
+    }
+}
