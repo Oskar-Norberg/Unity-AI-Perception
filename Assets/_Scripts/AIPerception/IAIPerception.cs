3db302baf788364b050837fdd27e9d24214a600c
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-07-16 11:38:40 +0200

    ADD: [Script] AIPerception

---
+++ Assets/_Scripts/AIPerception/IAIPerception.cs
@@ -0,0 +1,11 @@
+﻿using ringo.AIPerception.Senses;
+
+namespace ringo.AIPerception
+{
+    public interface IAIPerception
+    {
+        void SetActive(bool isActive);
+        
+        void NotifyPerceptionEvent<T>(IPerceptionData perceptionData) where T : IPerceptionSense;
+    }
+}
