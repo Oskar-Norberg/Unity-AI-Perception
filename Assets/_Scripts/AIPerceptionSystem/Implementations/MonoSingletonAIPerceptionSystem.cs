6b960dc1a3f7ce9144fd1635acca5a7085dee01e
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-07-16 11:38:26 +0200

    ADD: [Script] MonoSingletonAIPerceptionSystem

---
+++ Assets/_Scripts/AIPerceptionSystem/Implementations/MonoSingletonAIPerceptionSystem.cs
@@ -0,0 +1,41 @@
+﻿using ringo.AIPerception;
+using ringo.AIPerception.Senses;
+using UnityEngine;
+
+namespace ringo.AIPerceptionSystem
+{
+    public class MonoSingletonAIPerceptionSystem : MonoBehaviour, IAIPerceptionSystem
+    {
+        public static MonoSingletonAIPerceptionSystem Instance { get; private set; }
+        
+        private AIPerceptionSystem _perceptionSystem;
+
+        private void Awake()
+        {
+            if (!Instance)
+            {
+                Instance = this;
+                _perceptionSystem = new AIPerceptionSystem();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+        public void RegisterSense(IPerceptionSense sense, IAIPerception perception)
+        {
+            _perceptionSystem.RegisterSense(sense, perception);
+        }
+
+        public void UnregisterSense(IPerceptionSense sense, IAIPerception perception)
+        {
+            _perceptionSystem.UnregisterSense(sense, perception);
+        }
+
+        public void Alert<T>(IPerceptionData perceptionData) where T : IPerceptionSense
+        {
+            _perceptionSystem.Alert<T>(perceptionData);
+        }
+    }
+}
