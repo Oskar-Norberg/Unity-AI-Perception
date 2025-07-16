51c4ae1d7ec42469a3b72c872cbf09495edc7e09
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-07-16 11:39:31 +0200

    ADD: [Script] HearingSenseSO

---
+++ Assets/_Scripts/AIPerception/Senses/Hearing/HearingSenseSO.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace ringo.AIPerception.Senses.Hearing
+{
+    [CreateAssetMenu(fileName = "HearingData", menuName = "AI Senses/Hearing Sense Data")]
+    public class HearingSenseSO : AISenseSO
+    {
+        public float range = 10.0f;
+
+        public override bool ConditionMet(Transform perceiver, IPerceptionData perceptionData)
+        {
+            if (perceptionData is not HearingPerceptionData hearingData || !perceiver)
+            {
+                return false;
+            }
+
+
+            // Calculate distance from the perceiver to the sound source.
+            // TODO: Weird mix of Vector3 and System.Numerics.Vector3.
+            float distance = Vector3.Distance(perceiver.position,
+                new Vector3(hearingData.Position.X, hearingData.Position.Y, hearingData.Position.Z));
+            
+            return distance <= range;
+        }
+    }
+}
