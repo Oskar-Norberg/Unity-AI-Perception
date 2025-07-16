21ae32be370aac1d73c8a341812ec0baa98421ed
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-07-16 11:38:12 +0200

    ADD: [Script] IAIPerceptionSystem

---
+++ Assets/_Scripts/AIPerceptionSystem/IAIPerceptionSystem.cs
@@ -0,0 +1,13 @@
+﻿using ringo.AIPerception;
+using ringo.AIPerception.Senses;
+
+namespace ringo.AIPerceptionSystem
+{
+    public interface IAIPerceptionSystem
+    {
+        public void RegisterSense(IPerceptionSense sense, IAIPerception perception);
+        public void UnregisterSense(IPerceptionSense sense, IAIPerception perception);
+
+        public void Alert<T>(IPerceptionData perceptionData) where T : IPerceptionSense;
+    }
+}
